7ac8582aa5defd93dcd653270aea82cbcbbf973a
smakarychev <49116143+smakarychev@users.noreply.github.com>
2024-09-28 18:43:43 +0700

    optimizers

---
+++ src/RealFunctionOptimizationLib/Optimizators/GaussNewtonOptimizator.cpp
@@ -0,0 +1,38 @@
+#include "GaussNewtonOptimizator.h"
+
+#include "Functionals/IFunctional.h"
+#include "Functions/IFunction.h"
+
+namespace Optimizators
+{
+using namespace Functions;
+using namespace Functionals;
+
+GaussNewtonOptimizator::GaussNewtonOptimizator(uint32_t maxIterations, double maxResidual)
+    : OptimizatorBase(maxIterations, maxResidual)
+{
+}
+
+Vector GaussNewtonOptimizator::Minimize(IFunctional& objective, IParametricFunction& function,
+    const Vector& initialParameters, Vector* minimumParameters, Vector* maximumParameters)
+{
+    if (dynamic_cast<ILeastSquaresFunctional*>(&objective) == nullptr)
+        throw std::runtime_error("GaussNewtonOptimizator accepts only ILeastSquaresFunctional objective functions");
+
+    auto& leastSquaresFunctional = dynamic_cast<ILeastSquaresFunctional&>(objective);
+
+    Vector parameters = initialParameters;
+
+    for(uint32_t i = 0; i < m_maxIterations; i++)
+    {
+        Vector residual = leastSquaresFunctional.Residual(*function.Bind(parameters));
+        if (residual.norm() < m_maxResidual)
+            break;
+    
+        Matrix jacobianInverse = leastSquaresFunctional.Jacobian(*function.Bind(parameters)).inverse();
+        parameters = parameters - jacobianInverse * residual;
+    }
+
+    return parameters;
+}
+}
---
+++ src/RealFunctionOptimizationLib/Optimizators/MonteCarloOptimizator.cpp
@@ -0,0 +1,50 @@
+#include "MonteCarloOptimizator.h"
+
+#include "Functionals/IFunctional.h"
+#include "Functions/IFunction.h"
+
+namespace Optimizators
+{
+using namespace Functions;
+using namespace Functionals;
+
+MonteCarloOptimizator::MonteCarloOptimizator(uint32_t maxIterations, double maxResidual, uint32_t seed)
+    : OptimizatorBase(maxIterations, maxResidual), m_randomNumberGenerator(seed)
+{
+}
+
+Vector MonteCarloOptimizator::Minimize(IFunctional& objective, IParametricFunction& function,
+    const Vector& initialParameters, Vector* minimumParameters, Vector* maximumParameters)
+{
+    Vector parameters = initialParameters;
+    Vector minParameters = initialParameters;
+
+    std::vector<std::uniform_real_distribution<>> distributions;
+    distributions.reserve(parameters.size());
+    for (uint32_t i = 0; i < distributions.size(); i++)
+        distributions.emplace_back(
+            minimumParameters ? (*minimumParameters)[i] : -std::numeric_limits<double>::infinity(),
+            maximumParameters ? (*maximumParameters)[i] :  std::numeric_limits<double>::infinity());
+
+    double currentMin = objective.Value(*function.Bind(parameters));
+
+    for (uint32_t i = 0; i < m_maxIterations; i++)
+    {
+        // pick new parameters randomly
+        for (uint32_t paramIndex = 0; paramIndex < parameters.size(); paramIndex++)
+            parameters[paramIndex] = distributions[paramIndex](m_randomNumberGenerator);
+
+        const double val = objective.Value(*function.Bind(parameters));
+        if (val < currentMin)
+        {
+            currentMin = val;
+            minParameters = parameters;
+        }
+    
+        if (currentMin < m_maxResidual)
+            break;
+    }
+
+    return minParameters;
+}
+}
