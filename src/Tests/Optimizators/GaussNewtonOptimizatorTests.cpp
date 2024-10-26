d5b26a78be8b68bc96685429d218050b8c025d46
smakarychev <49116143+smakarychev@users.noreply.github.com>
2024-10-26 20:47:00 +0700

    GN optimizer tests

--- src/Tests/Optimizators/GaussNewtonOptimizatorTests.cpp
+++ src/Tests/Optimizators/GaussNewtonOptimizatorTests.cpp
@@ -1,6 +1,7 @@
 #include <gtest/gtest.h>
 
 #include "OptimizatorsTestsCommon.h"
+#include "Functions/InterpolationCubicSpline.h"
 #include "Functions/LineFunction.h"
 #include "Functions/PiecewiseLineFunction.h"
 #include "Optimizators/GaussNewtonOptimizator.h"
@@ -24,6 +25,24 @@ TEST_F(GaussNewtonL1NormOptimizationTests, Minimize_WithLineFunctionAndL1Functio
     ASSERT_THROW(optimizator->Minimize(*LineFunctional, lineFunction, LINE_INITIAL_PARAMETERS), std::runtime_error);
 }
 
+TEST_F(GaussNewtonL1NormOptimizationTests, Minimize_WithPiecewiseLineFunctionAndL1Functional_ShouldThrowException)
+{
+    std::unique_ptr<IOptimizator> optimizator =
+        std::make_unique<GaussNewtonOptimizator>(MaxIterations, MaxResidual);
+    PiecewiseLineFunction lineFunction{SPLINE_ARGUMENTS};
+    
+    ASSERT_THROW(optimizator->Minimize(*LineFunctional, lineFunction, SPLINE_INITIAL_PARAMETERS), std::runtime_error);
+}
+
+TEST_F(GaussNewtonL1NormOptimizationTests, Minimize_WithCubicSplineFunctionAndL1Functional_ShouldThrowException)
+{
+    std::unique_ptr<IOptimizator> optimizator =
+        std::make_unique<GaussNewtonOptimizator>(MaxIterations, MaxResidual);
+    InterpolationCubicSpline lineFunction{CUBIC_SPLINE_ARGUMENTS};
+    
+    ASSERT_THROW(optimizator->Minimize(*LineFunctional, lineFunction, CUBIC_SPLINE_INITIAL_PARAMETERS), std::runtime_error);
+}
+
 TEST_F(GaussNewtonL2NormOptimizationTests, Minimize_WithLineFunctionAndL2Functional_ShouldMinimize)
 {
     std::unique_ptr<IOptimizator> optimizator =
@@ -34,6 +53,28 @@ TEST_F(GaussNewtonL2NormOptimizationTests, Minimize_WithLineFunctionAndL2Functio
     EXPECT_NEAR(parameters[0], GetMinimizingLineYIsX(), TOLERANCE);
 }
 
+TEST_F(GaussNewtonL2NormOptimizationTests, Minimize_WithPiecewiseLineFunctionAndL2Functional_ShouldMinimize)
+{
+    std::unique_ptr<IOptimizator> optimizator =
+        std::make_unique<GaussNewtonOptimizator>(MaxIterations, MaxResidual);
+
+    PiecewiseLineFunction lineFunction{SPLINE_ARGUMENTS};
+    Vector parameters = optimizator->Minimize(*SplineFunctional, lineFunction, SPLINE_INITIAL_PARAMETERS);
+
+    for (uint32_t i = 0; i < parameters.size(); i++)
+        EXPECT_NEAR(parameters[i], FunctionYIsAbsXTable[i].value, TOLERANCE);
+}
+
+TEST_F(GaussNewtonL2NormOptimizationTests, Minimize_WithInterpolationCubicSplineFunctionAndL2Functional_ShouldThrowException)
+{
+    std::unique_ptr<IOptimizator> optimizator =
+        std::make_unique<GaussNewtonOptimizator>(MaxIterations, MaxResidual);
+
+    InterpolationCubicSpline cubicSpline{CUBIC_SPLINE_ARGUMENTS};
+
+    ASSERT_THROW(optimizator->Minimize(*LineFunctional, cubicSpline, CUBIC_SPLINE_INITIAL_PARAMETERS), std::runtime_error);
+}
+
 TEST_F(GaussNewtonLInfNormOptimizationTests, Minimize_WithLineFunctionAndLInfFunctional_ShouldThrowException)
 {
     std::unique_ptr<IOptimizator> optimizator =
@@ -41,4 +82,22 @@ TEST_F(GaussNewtonLInfNormOptimizationTests, Minimize_WithLineFunctionAndLInfFun
     LineFunction lineFunction;
 
     ASSERT_THROW(optimizator->Minimize(*LineFunctional, lineFunction, LINE_INITIAL_PARAMETERS), std::runtime_error);
+}
+
+TEST_F(GaussNewtonLInfNormOptimizationTests, Minimize_WithPiecewiseLineFunctionAndLInfFunctional_ShouldThrowException)
+{
+    std::unique_ptr<IOptimizator> optimizator =
+        std::make_unique<GaussNewtonOptimizator>(MaxIterations, MaxResidual);
+    PiecewiseLineFunction lineFunction{SPLINE_ARGUMENTS};
+    
+    ASSERT_THROW(optimizator->Minimize(*LineFunctional, lineFunction, SPLINE_INITIAL_PARAMETERS), std::runtime_error);
+}
+
+TEST_F(GaussNewtonLInfNormOptimizationTests, Minimize_WithCubicSplineFunctionAndLInfFunctional_ShouldThrowException)
+{
+    std::unique_ptr<IOptimizator> optimizator =
+        std::make_unique<GaussNewtonOptimizator>(MaxIterations, MaxResidual);
+    InterpolationCubicSpline lineFunction{CUBIC_SPLINE_ARGUMENTS};
+    
+    ASSERT_THROW(optimizator->Minimize(*LineFunctional, lineFunction, CUBIC_SPLINE_INITIAL_PARAMETERS), std::runtime_error);
 }
