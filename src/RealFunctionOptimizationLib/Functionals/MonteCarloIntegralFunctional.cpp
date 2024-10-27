62a63706c69c083a8021de482915b3b73decc1df
smakarychev <49116143+smakarychev@users.noreply.github.com>
2024-10-27 17:33:51 +0700

    Monte-carlo integral optimization tests (#21)
    * monte-carlo integration tests
    
    * monte-carlo integral optimization tests

--- src/RealFunctionOptimizationLib/Functionals/MonteCarloIntegralFunctional.cpp
+++ src/RealFunctionOptimizationLib/Functionals/MonteCarloIntegralFunctional.cpp
@@ -2,35 +2,36 @@
 
 #include "Functions/IFunction.h"
 
-Functionals::MonteCarloIntegralFunctional::MonteCarloIntegralFunctional(const IntegralHyperBoxDomain& domain,
-    uint32_t samples, uint32_t seed,
-    const std::vector<FunctionPointAndValue>& functionPointValues)
-        : FunctionalBase(functionPointValues),
-    m_hyperBoxDomain(domain),
-    m_samples(samples), m_randomNumberGenerator(seed)
+namespace Functionals
+{
+MonteCarloIntegralFunctional::MonteCarloIntegralFunctional(const IntegralHyperBoxDomain& domain,
+    uint32_t samples, uint32_t seed)
+        : m_hyperBoxDomain(domain), m_samples(samples), m_randomNumberGenerator(seed)
 {
 }
 
-double Functionals::MonteCarloIntegralFunctional::Value(Functions::IFunction& function)
+double MonteCarloIntegralFunctional::Value(Functions::IFunction& function)
 {
-    double pdf = 1.0f;
+    double pdf = 1.0;
     std::vector<std::uniform_real_distribution<>> distributions;
     distributions.reserve(m_hyperBoxDomain.MinParameters.size());
-    for (uint32_t i = 0; i < distributions.size(); i++)
+    for (uint32_t i = 0; i < m_hyperBoxDomain.MinParameters.size(); i++)
     {
         pdf *= m_hyperBoxDomain.MaxParameters[i] - m_hyperBoxDomain.MinParameters[i];
         distributions.emplace_back(m_hyperBoxDomain.MinParameters[i], m_hyperBoxDomain.MaxParameters[i]);
     }
+    pdf = 1.0 / pdf;
 
     Vector samplePoint = Vector::Zero(distributions.size());
-    double value = 0.0f;
+    double value = 0.0;
     for (uint32_t sample = 0; sample < m_samples; sample++)
     {
         for (uint32_t i = 0; i < samplePoint.size(); i++)
             samplePoint[i] = distributions[i](m_randomNumberGenerator);
-        
+    
         value += function.Value(samplePoint);
     }
 
     return value / (pdf * m_samples);
+}
 }
--- src/Tests/Optimizators/OptimizatorsTestsCommon.h
+++ src/Tests/Optimizators/OptimizatorsTestsCommon.h
@@ -5,6 +5,8 @@
 #include "Functionals/L1NormDifferenceFunctional.h"
 #include "Functionals/L2NormDifferenceFunctional.h"
 #include "Functionals/LInfNormDifferenceFunctional.h"
+#include "Functionals/MonteCarloIntegralFunctional.h"
+#include "Functions/IFunction.h"
 
 static const Vector LINE_INITIAL_PARAMETERS = Vector::Constant(1, 0.5);
 static const Vector SPLINE_ARGUMENTS = Vector{{-1.0, 0.0, 1.0}};
@@ -61,3 +63,52 @@ public:
 using L1NormOptimizationTests = FunctionalOptimizationTests<Functionals::L1NormDifferenceFunctional>;
 using L2NormOptimizationTests = FunctionalOptimizationTests<Functionals::L2NormDifferenceFunctional>;
 using LInfNormOptimizationTests = FunctionalOptimizationTests<Functionals::LInfNormDifferenceFunctional>;
+
+
+static const Vector INTEGRAL_DOMAIN_MIN = Vector{{0.0}};
+static const Vector INTEGRAL_DOMAIN_MAX = Vector{{1.0}};
+static const uint32_t INTEGRAL_SAMPLES = 1'000;
+static const uint32_t INTEGRAL_SEED = 0;
+static const Vector INTEGRAL_PARAMETERS = Vector{{0.0}};
+
+class IntegralFunctionalOptimizationTests : public testing::Test
+{
+public:
+    // function is (x^2 - a)^2, where a is a parameter
+    class IntegralFunction : public Functions::IParametricFunction
+    {
+    public:
+        std::unique_ptr<Functions::IFunction> Bind(const Vector& parameters) override
+        {
+            return std::make_unique<InternalIntegralFunction>(parameters);
+        }
+    private:
+        class InternalIntegralFunction : public Functions::IFunction
+        {
+        public:
+            InternalIntegralFunction(const Vector& parameters) : m_parameters(parameters) {}
+            double Value(const Vector& point) override
+            {
+                double val = point[0] * point[0] - m_parameters[0];
+                return val * val;
+            }
+        private:
+            Vector m_parameters;
+        };
+    };
+public:
+    uint32_t MaxIterations{100'000};
+    double MaxResidual{1e-4};
+    std::unique_ptr<Functionals::MonteCarloIntegralFunctional> Functional;
+    void SetUp() override
+    {
+        Functional = std::make_unique<Functionals::MonteCarloIntegralFunctional>(
+            Functionals::IntegralHyperBoxDomain{.MinParameters = INTEGRAL_DOMAIN_MIN, .MaxParameters = INTEGRAL_DOMAIN_MAX},
+            INTEGRAL_SAMPLES, INTEGRAL_SEED);
+    }
+    double GetExpectedMin() const
+    {
+        // https://www.youtube.com/watch?v=2Tz8KzMiVOY  the min of integral_0_1((x^2 - a)^2) is 4/45, with the value of a being 1/3
+        return 1.0 / 3.0;
+    }
+};
